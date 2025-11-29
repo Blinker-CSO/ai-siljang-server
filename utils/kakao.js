b992dac170cfef5c57628ab56d8874c4734b8464
Blinker-CSO <greth.ceo@gmail.com>
2025-11-29 23:01:09 +0900

    Update kakao.js

--- utils/kakao.js
+++ utils/kakao.js
@@ -1,20 +1,22 @@
 import { SolapiMessageService } from "solapi";
 
 const messageService = new SolapiMessageService(
-  process.env.SOLAPI_KEY,
-  process.env.SOLAPI_SECRET
+  "NCSN0EQ2GDDQLPJT",     // SOLAPI_KEY
+  "NNMVVFALJRUHTHJYAPAAN1O3YTQOQLDX"  // SOLAPI_SECRET
 );
 
 export async function sendKakao(to, text) {
   try {
     const result = await messageService.sendOne({
       to,
-      from: process.env.SENDER_NUMBER,
+      from: "01077778518",   // 발신번호 (하이픈 없이)
       text
     });
+
+    console.log("📨 문자 발송 성공:", result);
     return result;
   } catch (err) {
-    console.error("SMS 발송 오류:", err);
+    console.error("❌ 문자 발송 오류:", err);
     return false;
   }
 }
