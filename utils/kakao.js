fba3a80cd699f8fe6446102c3590eb233ba989bb
Blinker-CSO <greth.ceo@gmail.com>
2025-11-29 23:37:14 +0900

    Update kakao.js

--- utils/kakao.js
+++ utils/kakao.js
@@ -2,31 +2,30 @@ import axios from "axios";
 
 export async function sendSMS(to, text) {
   try {
-    const res = await axios({
-      method: "post",
-      url: "https://api.solapi.com/messages/v4/send",
-      headers: {
-        Authorization:
-          "HMAC-SHA256 ApiKey=" +
-          process.env.SOLAPI_KEY +
-          ", Date=" +
-          new Date().toISOString(),
-        "Content-Type": "application/json"
-      },
-      data: {
+    const res = await axios.post(
+      "https://api.solapi.com/messages/v4/send",
+      {
         message: {
           to: to,
           from: process.env.SENDER_NUMBER,
           text: text
         }
+      },
+      {
+        headers: {
+          Authorization:
+            `HMAC-SHA256 ApiKey=${process.env.SOLAPI_KEY}, Date=${new Date().toISOString()}`,
+          "Content-Type": "application/json"
+        }
       }
-    });
+    );
 
     return true;
   } catch (error) {
     console.error("🔥 [SMS 전송 실패 상세]", {
       status: error.response?.status,
-      data: error.response?.data
+      data: error.response?.data,
+      msg: error.message
     });
 
     return false;
